b70499e7203bef9685e1c7a24bf50345f7f0087c
Joe Cooper <joecooper2991@gmail.com>
2020-06-01 12:18:39 +0100

    add parameters and defaults

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-  };
+module.exports = function drawStainedGlass(
+  ctx,
+  height,
+  width,
+  {
+    pieceSize = 25,
+    lineWidth = 1,
+    lineColor = "black",
+    deviation = 25,
+    shape = "abstract",
+    colorScheme = "rgb",
+    colorConsistency = 30,
+    transparency = 1,
+    scatter = 2,
+    angle = 0,
+  } = {}
+) {};
