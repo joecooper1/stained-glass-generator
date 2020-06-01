f37433e1bf2b03af95f1c034d19664811c052b02
Joe Cooper <joecooper2991@gmail.com>
2020-06-01 13:31:20 +0100

    add drawCirlce function

---
+++ utils/drawCircle.js
@@ -0,0 +1,12 @@
+//Draw circle
+export const drawCircle = (pane, colorScheme, ctx, pieceWidth, transparency) => {
+  //Set fill style and stroke style and line width
+  if (colorScheme === "hsl") {
+    ctx.fillStyle = `hsla(${pane.colorsHSL.h}, ${pane.colorsHSL.s}%, ${pane.colorsHSL.l}%, ${transparency})`;
+  } else {
+    ctx.fillStyle = `rgba(${pane.colorsRGB.r}, ${pane.colorsRGB.g}, ${pane.colorsRGB.b}, ${transparency})`;
+  }
+  ctx.beginPath();
+  ctx.arc(pane.coords.x, pane.coords.y, pieceWidth / 2, 0, Math.PI * 2);
+  ctx.fill();
+};
