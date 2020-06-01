ab8a7b8d04f65b516fef921eed92903bfae0134f
Joe Cooper <joecooper2991@gmail.com>
2020-06-01 13:36:28 +0100

    export more files

--- utils/exports.js
+++ utils/exports.js
@@ -1,4 +1,17 @@
 import getRandomNumber from "./getRandomNum";
-import makePanes from './makePanes'
+import makePanes from "./makePanes";
+import setColors from "./setColors";
+import scatterPanes from "./scatterPanes";
+import drawPolygon from "./drawPolygon";
+import drawCircle from "./drawCircle";
+import drawRing from "./drawRings";
 
-export {getRandomNumber, makePanes};
+export {
+  getRandomNumber,
+  makePanes,
+  setColors,
+  scatterPanes,
+  drawPolygon,
+  drawCircle,
+  drawRing,
+};
