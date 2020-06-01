becebdd4a435b585856befd2f4b9c23b880333c8
Joe Cooper <joecooper2991@gmail.com>
2020-06-01 13:32:36 +0100

    add drawRings function

---
+++ utils/drawRings.js
@@ -0,0 +1,14 @@
+ //Draw rings
+ export const drawRing = (pane, i, j, colorScheme, ctx, transparency, pieceWidth) => {
+    //Set fill style and stroke style and line width
+    if (colorScheme === "hsl") {
+      ctx.fillStyle = `hsla(${pane.colorsHSL.h}, ${pane.colorsHSL.s}%, ${pane.colorsHSL.l}%, ${transparency})`;
+    } else {
+      ctx.fillStyle = `rgba(${pane.colorsRGB.r}, ${pane.colorsRGB.g}, ${pane.colorsRGB.b}, ${transparency})`;
+    }
+    if ((i + j) % 2 === 0) {
+      ctx.beginPath();
+      ctx.arc(pane.coords.x, pane.coords.y, pieceWidth, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  };
