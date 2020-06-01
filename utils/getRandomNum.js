44cdcf37634b51604a975e51a3d4d7705552941e
Joe Cooper <joecooper2991@gmail.com>
2020-06-01 12:56:47 +0100

    change export to default

--- utils/getRandomNum.js
+++ utils/getRandomNum.js
@@ -1,4 +1,4 @@
 //Get random number within range
-export const getRandomNumber = (range = 30) => {
+export default getRandomNumber = (range = 30) => {
   return Math.floor(Math.random() * (range + 1) - range / 2);
 };
